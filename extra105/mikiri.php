3f78430f4b1122e01864699d7c201fa37c874c2a
DESKTOP-IG7K16R\er-GOD-ic <kamepower@outlook.jp>
2024-09-28 14:56:34 +0900

    eric: add animation file

--- extra105/mikiri.php
+++ extra105/mikiri.php
@@ -75,7 +75,7 @@ $username = $_SESSION['username'];
             <img src="keyboard/hyphen.png" id="hyphen" alt="hypen" style="display:none;height:auto">
             <img src="keyboard/leter.png" alt="leter" style = "height:auto">
         </div>
-
+        <script src="mikiri_animations.js"></script>
         <script>
         $(document).ready(function() {
             let matchId = null;
---
+++ extra105/mikiri_animations.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    function animateBackground() {
+        $('.background').each(function(index, element) {
+            $(element).animate({
+                left: '+=100px'
+            }, 5000, function() {
+                $(element).css('left', '-100px');
+                animateBackground();
+            });
+        });
+    }
+
+    animateBackground();
+});
