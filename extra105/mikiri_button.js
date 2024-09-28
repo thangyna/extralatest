8fda67fe7c10ccbd19452a54eae03771fb14e6d7
DESKTOP-IG7K16R\er-GOD-ic <kamepower@outlook.jp>
2024-09-28 14:24:26 +0900

    eric: add mikiri_button.js

---
+++ extra105/mikiri_button.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const playButton = document.getElementById('playButton');
+    const quitButton = document.getElementById('quitButton');
+    const playButtonImage = document.getElementById('playButtonImage');
+    const quitButtonImage = document.getElementById('quitButtonImage');
+
+    function changeImage(button) {
+        if (button === 'play') {
+            playButtonImage.src = 'button/play_down.png';
+        } else if (button === 'quit') {
+            quitButtonImage.src = 'button/quit_down.png';
+        }
+    }
+
+    function resetImage(button) {
+        if (button === 'play') {
+            playButtonImage.src = 'button/play.png';
+        } else if (button === 'quit') {
+            quitButtonImage.src = 'button/quit.png';
+        }
+    }
+    
+    // {}内がボタンが押されたときの処理
+    // マウスのボタンが押されたときの処理
+    playButton.addEventListener('mousedown', () => {
+        changeImage('play');
+        // ここから関数を呼ぶ
+    });
+    // マウスのボタンが離されたときの処理
+    playButton.addEventListener('mouseup', () => resetImage('play'));
+    // マウスがボタンから離れたときの処理
+    playButton.addEventListener('mouseleave', () => resetImage('play'));
+
+    quitButton.addEventListener('mousedown', () => changeImage('quit'));
+    quitButton.addEventListener('mouseup', () => resetImage('quit'));
+    quitButton.addEventListener('mouseleave', () => resetImage('quit'));
+});
