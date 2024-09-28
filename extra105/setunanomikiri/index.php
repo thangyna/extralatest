c11082c7a696946ebb928b405519de365107c1d7
DESKTOP-IG7K16R\er-GOD-ic <kamepower@outlook.jp>
2024-09-28 14:01:41 +0900

    eric: add background img

--- extra105/setunanomikiri/index.php
+++ extra105/setunanomikiri/index.php
@@ -12,23 +12,44 @@ $username = $_SESSION['username'];
     <title>刹那の見切り - キーボード版</title>
     <link rel="stylesheet" type="text/css" href="/mikiri_style.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+        body {
+            overflow: hidden;
+            background-color: #73A2AD;
+        }
+    </style>
 </head>
 <body>
-    <div class="header">
-        <span>ログイン中: <?php echo htmlspecialchars($username); ?></span>
-        <a href="../toppage.php">トップページに戻る</a>
-    </div>
-    <h1>刹那の見切り - キーボード版</h1>
-    <div id="game-area">
-        <div id="status">マッチメイキングを開始してください。</div>
-        <button id="matchmaking-button">マッチメイキング開始</button>
-        <div id="game-display" style="display:none;">
-            <div id="message"></div>
-            <div id="letter" style="font-size: 48px;"></div>
-            <input type="text" id="input" maxlength="1" style="display:none;">
+    <div class="sky-contaier">
+        <div id="background" style="background-image:url(asset/sky.png)">
+        <div id="background" style="background-image:url(asset/forest_4.png)">
+        <div id="background" style="background-image:url(asset/forest_3.png)">
+        <div id="background" style="background-image:url(asset/forest_2.png)">
+        <div id="background" style="background-image:url(asset/forest_1.png)">
+        <div class="ground">
+            <div class="header">
+                <span>ログイン中: <?php echo htmlspecialchars($username); ?></span>
+                <a href="../toppage.php">トップページに戻る</a>
+            </div>
+            <h1>刹那の見切り - キーボード版</h1>
+            <div id="game-area">
+                <div id="status">マッチメイキングを開始してください。</div>
+                <button id="matchmaking-button">マッチメイキング開始</button>
+                <div id="game-display" style="display:none;">
+                    <div id="message"></div>
+                    <div id="letter" style="font-size: 48px;"></div>
+                    <input type="text" id="input" maxlength="1" style="display:none;">
+                </div>
+                <div id="timer" style="display:none;"></div>
+            </div>
+            <script src="game.js"></script>
         </div>
-        <div id="timer" style="display:none;"></div>
+        </div>
+        </div>
+        </div>
     </div>
-    <script src="game.js"></script>
 </body>
 </html>
