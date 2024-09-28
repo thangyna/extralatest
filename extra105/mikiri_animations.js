48402da3790be1ff7a07c2ec6067f57117cfc2ec
DESKTOP-IG7K16R\er-GOD-ic <kamepower@outlook.jp>
2024-09-28 15:11:26 +0900

    eric: animate

--- extra105/mikiri_animations.js
+++ extra105/mikiri_animations.js
@@ -7,13 +7,17 @@ function scrollBackground() {
     var elem4 = document.getElementById("background_4");
     var sky = document.getElementById("sky");
 
-    posX -= 1;  // 背景を左に1ピクセルずつ移動
+    posX1 -= 1;  // 背景を左に1ピクセルずつ移動
+    posX2 -= 2;
+    posX3 -= 3;
+    posX4 -= 4;
+    skyX -= 5;
 
-    elem1.style.backgroundPosition = posX + "px 0px";  // 背景の位置を更新
-    elem2.style.backgroundPosition = posX/2 + "px 0px";
-    elem3.style.backgroundPosition = posX/3 + "px 0px";
-    elem4.style.backgroundPosition = posX/4 + "px 0px";
-    sky.style.backgroundPosition = posX/5 + "px 0px";
+    elem1.style.backgroundPosition = posX1 + "px 0px";  // 背景の位置を更新
+    elem2.style.backgroundPosition = posX2 + "px 0px";
+    elem3.style.backgroundPosition = posX3 + "px 0px";
+    elem4.style.backgroundPosition = posX4 + "px 0px";
+    sky.style.backgroundPosition = skyX + "px 0px";
 
     requestAnimationFrame(scrollBackground);  // アニメーションを継続
 }
