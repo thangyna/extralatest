4c8c658e7bd746356a1f1a21ca34a63afff0646f
DESKTOP-IG7K16R\er-GOD-ic <kamepower@outlook.jp>
2024-09-29 11:18:02 +0900

    eric: use local db

--- extra105/db.php
+++ extra105/db.php
@@ -1,8 +1,16 @@
 <?php
 $host = 'localhost';
-$dbname = 'cf760008_typing';
-$username = 'root';
-$password = 'makoto66';
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $username = 'root';
+    $password = 'obrien'; // or your password
+    $dbname = 'typing_game';
+    $conn = new mysqli($host, $username, $password, $dbname);
+}
+else {
+    $username = 'root';
+    $password = 'makoto66';
+    $dbname = 'cf760008_typing';
+}
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
