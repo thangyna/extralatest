c45e3d1e39856e7ca4a51607697ba5307562ddfd
DESKTOP-IG7K16R\er-GOD-ic <kamepower@outlook.jp>
2024-09-29 16:01:20 +0900

    re commit

--- extra105/db.php
+++ extra105/db.php
@@ -3,15 +3,15 @@ $host = 'localhost';
 if ($_SERVER['HTTP_HOST'] == 'localhost') {
     $username = 'root';
     $password = 'obrien'; // or your password
-    $dbname = 'typing_game';
-    $conn = new mysqli($host, $username, $password, $dbname);
+    $dbname = 'cf760008_typing';
 }
 else {
     $username = 'cf760008_aioi';
     $password = 'makoto66';
     $dbname = 'cf760008_typing';
-    $conn = new mysqli($host, $username, $password, $dbname);
 }
+
+$conn = new mysqli($host, $username, $password, $dbname);
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
